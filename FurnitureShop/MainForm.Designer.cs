a85e125ab80142b8fc2ffa5ec9c8f9d1ab7e94ee
rilvean <rilveanya@gmail.com>
2025-11-12 23:06:15 +0300

    Added login history

--- FurnitureShop/MainForm.Designer.cs
+++ FurnitureShop/MainForm.Designer.cs
@@ -33,9 +33,15 @@
 			tabMaterialsManagement = new TabPage();
 			tabProductsAnalysation = new TabPage();
 			tabLoginHistory = new TabPage();
+			label1 = new Label();
+			dataLoginHistory = new DataGridView();
 			picLogo = new PictureBox();
 			lblUserInfo = new Label();
+			lblLogo = new Label();
+			txtFilterByLogin = new TextBox();
 			tabControlMain.SuspendLayout();
+			tabLoginHistory.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)dataLoginHistory).BeginInit();
 			((System.ComponentModel.ISupportInitialize)picLogo).BeginInit();
 			SuspendLayout();
 			// 
@@ -83,12 +89,33 @@
 			// 
 			// tabLoginHistory
 			// 
+			tabLoginHistory.Controls.Add(txtFilterByLogin);
+			tabLoginHistory.Controls.Add(label1);
+			tabLoginHistory.Controls.Add(dataLoginHistory);
 			tabLoginHistory.Location = new Point(4, 35);
 			tabLoginHistory.Name = "tabLoginHistory";
 			tabLoginHistory.Size = new Size(1176, 641);
 			tabLoginHistory.TabIndex = 3;
 			tabLoginHistory.Text = "Login History";
 			tabLoginHistory.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(263, 562);
+			label1.Name = "label1";
+			label1.Size = new Size(150, 27);
+			label1.TabIndex = 1;
+			label1.Text = "Filter by login:";
+			// 
+			// dataLoginHistory
+			// 
+			dataLoginHistory.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataLoginHistory.Dock = DockStyle.Top;
+			dataLoginHistory.Location = new Point(0, 0);
+			dataLoginHistory.Name = "dataLoginHistory";
+			dataLoginHistory.Size = new Size(1176, 513);
+			dataLoginHistory.TabIndex = 0;
 			// 
 			// picLogo
 			// 
@@ -103,24 +130,47 @@
 			// 
 			// lblUserInfo
 			// 
+			lblUserInfo.Anchor = AnchorStyles.Top | AnchorStyles.Right;
 			lblUserInfo.AutoSize = true;
-			lblUserInfo.Location = new Point(919, 29);
+			lblUserInfo.Location = new Point(1077, 9);
 			lblUserInfo.Name = "lblUserInfo";
 			lblUserInfo.Size = new Size(95, 27);
 			lblUserInfo.TabIndex = 2;
 			lblUserInfo.Text = "UserInfo";
+			// 
+			// lblLogo
+			// 
+			lblLogo.AutoSize = true;
+			lblLogo.Font = new Font("Constantia", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			lblLogo.Location = new Point(80, 9);
+			lblLogo.Name = "lblLogo";
+			lblLogo.Size = new Size(252, 59);
+			lblLogo.TabIndex = 3;
+			lblLogo.Text = "Obraz Plus";
+			// 
+			// txtFilterByLogin
+			// 
+			txtFilterByLogin.Location = new Point(419, 555);
+			txtFilterByLogin.Name = "txtFilterByLogin";
+			txtFilterByLogin.Size = new Size(354, 34);
+			txtFilterByLogin.TabIndex = 2;
+			txtFilterByLogin.TextChanged += txtFilterByLogin_TextChanged;
 			// 
 			// MainForm
 			// 
 			AutoScaleDimensions = new SizeF(96F, 96F);
 			AutoScaleMode = AutoScaleMode.Dpi;
 			ClientSize = new Size(1184, 761);
+			Controls.Add(lblLogo);
 			Controls.Add(lblUserInfo);
 			Controls.Add(picLogo);
 			Controls.Add(tabControlMain);
 			Name = "MainForm";
 			Text = "Obraz Plus - MainForm";
 			tabControlMain.ResumeLayout(false);
+			tabLoginHistory.ResumeLayout(false);
+			tabLoginHistory.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)dataLoginHistory).EndInit();
 			((System.ComponentModel.ISupportInitialize)picLogo).EndInit();
 			ResumeLayout(false);
 			PerformLayout();
@@ -135,5 +185,9 @@
 		private Label lblUserInfo;
 		private TabPage tabProductsAnalysation;
 		private TabPage tabLoginHistory;
+		private Label lblLogo;
+		private DataGridView dataLoginHistory;
+		private Label label1;
+		private TextBox txtFilterByLogin;
 	}
 }
--- FurnitureShop/MainForm.cs
+++ FurnitureShop/MainForm.cs
@@ -1,4 +1,7 @@
 ﻿using FurnitureShop.Data;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace FurnitureShop
 {
@@ -19,7 +22,41 @@ namespace FurnitureShop
 		private void MainForm_Load(object? sender, EventArgs e)
 		{
 			lblUserInfo.Text = $"{user.FullName} ({user.Role})";
+			lblUserInfo.ForeColor = AccentColor;
+			lblLogo.ForeColor = AccentColor;
+			LoadLoginHistory();
 		}
+
+		private void LoadLoginHistory(string loginFilter = null!)
+		{
+			FormatDataGridView(dataLoginHistory, MainColor, AdditionalColor, AccentColor, BaseFont);
+
+			using (Context db = new())
+			{
+				var query = db.EntranceLogs
+					.Select(l => new
+					{
+						l.LogInTime,
+						l.FullName,
+						l.Role,
+						Login = l.User.Login
+					});
+
+				if (!string.IsNullOrEmpty(loginFilter))
+				{
+					query = query.Where(l => l.Login.Contains(loginFilter));
+				}
+
+				var logs = query
+					.OrderByDescending(l => l.LogInTime)
+					.ToList();
+
+				dataLoginHistory.DataSource = logs;
+			}
+
+			dataLoginHistory.AutoResizeColumns();
+		}
+
 
 		protected override void ConfigureForm()
 		{
@@ -31,6 +68,39 @@ namespace FurnitureShop
 		{
 			IsLogout = true;
 			Logout?.Invoke(this, EventArgs.Empty);
+		}
+
+		public static void FormatDataGridView(DataGridView dataGridView, Color backColor, Color additionalColor, Color accentColor, Font font)
+		{
+			dataGridView.ReadOnly = true;
+			dataGridView.RowHeadersVisible = false;
+			dataGridView.AllowUserToAddRows = false;
+			dataGridView.AllowUserToDeleteRows = false;
+			dataGridView.AllowUserToResizeColumns = false;
+			dataGridView.AllowUserToResizeRows = false;
+
+			dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+			dataGridView.MultiSelect = false;
+
+			dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+			dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+			dataGridView.BackgroundColor = backColor;
+			dataGridView.BorderStyle = BorderStyle.None;
+
+			dataGridView.EnableHeadersVisualStyles = false;
+			dataGridView.ColumnHeadersDefaultCellStyle.BackColor = accentColor;
+			dataGridView.ColumnHeadersDefaultCellStyle.ForeColor = additionalColor;
+			dataGridView.ColumnHeadersDefaultCellStyle.SelectionBackColor = dataGridView.ColumnHeadersDefaultCellStyle.BackColor;
+			dataGridView.ColumnHeadersDefaultCellStyle.SelectionForeColor = dataGridView.ColumnHeadersDefaultCellStyle.ForeColor;
+
+			dataGridView.DefaultCellStyle.SelectionBackColor = additionalColor;
+			dataGridView.DefaultCellStyle.SelectionForeColor = accentColor;
+		}
+
+		private void txtFilterByLogin_TextChanged(object sender, EventArgs e)
+		{
+			LoadLoginHistory(txtFilterByLogin.Text.Trim());
 		}
 	}
 }
